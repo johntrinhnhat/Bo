45fd742be2d7e8dbd404f193f58ab7b49c2ec611
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-09-08 02:56:37 +0700

    Update App

--- app.py
+++ app.py
@@ -389,7 +389,7 @@ def main():
 
         if st.button("Tải Zip tự động"):
             chrome_options = webdriver.ChromeOptions()
-            # chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--headless")
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_experimental_option("prefs", {
