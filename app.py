8f03df11d1720492f779d4284c39a93c9b4f383f
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-09-08 00:13:28 +0700

    Update App

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ import zipfile
 from io import BytesIO
 from openpyxl import load_workbook
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
@@ -381,10 +381,16 @@ def main():
             end_date = st.date_input("Ngày kết thúc",  format="DD/MM/YYYY")
 
         if st.button("Tải Zip tự động"):
-            chrome_options = Options()
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument("--headless")
             chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--disable-dev-shm-usage')
-            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+            chrome_options.add_argument("--disable-dev-shm-usage")
+
+            # Point the browser to the correct location
+            chrome_options.binary_location = "/usr/bin/chromium"
+
+            # Use chromedriver installed by the system package manager
+            driver = webdriver.Chrome(service=Service("/usr/bin/chromedriver"), options=chrome_options)
             action=ActionChains(driver,10)
             wait = WebDriverWait(driver, 10)
 
