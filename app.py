18650e82dc8dbe9db15c790c835bd8e7f34c9309
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-09-13 15:08:21 +0700

    Update App

--- app.py
+++ app.py
@@ -588,7 +588,7 @@ def main():
                     time.sleep(2)
 
                     search_btn = wait.until(
-                        EC.presence_of_all_elements_located((By.CLASS_NAME, "btn btn-primary ng-star-inserted"))
+                        EC.presence_of_element_located((By.XPATH, "//button[span[text()='Tìm kiếm']]"))
                     )
                     # search_btn.click()
                     st.write_stream(stream_data(("Đang tìm hóa đơn ...")))
