a71fedab37ff79e3ccb05be3bd98d4c0c3b7102d
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-09-17 09:42:46 +0700

    Update App

--- app.py
+++ app.py
@@ -395,7 +395,7 @@ def handle_vnpt_download(driver, action, wait, user, start_date, end_date, temp_
                 dropdown = invoice_status.find_element(By.XPATH, "//div[@aria-label='Lựa chọn']")
                 st.write(f"dropdown: {dropdown}")
                 dropdown.click()
-                option = driver.find_element(By.XPATH, "//div[@title='Hóa đơn gốc']")
+                option = dropdown.find_element(By.XPATH, "//div[@title='Hóa đơn gốc']")
                 st.write(f"option: {option}")
                 option.click()
                 time.sleep(1)
