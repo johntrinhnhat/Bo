ac4d8ecd077d7302cd67ace4141e983a5ee8363f
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-09-17 10:37:10 +0700

    Update App

--- app.py
+++ app.py
@@ -398,7 +398,7 @@ def handle_vnpt_download(driver, action, wait, user, start_date, end_date, temp_
                 driver.execute_script("arguments[0].click();", dropdown)
 
                 pop_up_content = wait.until(
-                    EC.visibility_of_element_located((By.XPATH, "//div[@role='listbox']"))  # Adjust the XPath to target the pop-up container
+                    EC.presence_of_element_located((By.XPATH, "//div[@role='listbox']"))  # Adjust the XPath to target the pop-up container
                 )
 
                 
