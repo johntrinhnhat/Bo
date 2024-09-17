79f51a1e8fada8d64bcf22bc3e7b3adecba08b8c
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-09-17 12:33:45 +0700

    Update App

--- app.py
+++ app.py
@@ -299,6 +299,7 @@ def download_icon_vnpt(driver, action, wait, temp_folder):
         icons = icons[:len(icons)//2]  
         seen_files = set()
         st.write_stream(stream_data("Đang tải hóa đơn ..."))
+        
         for icon in icons:
             # Move to the icon and click to open details
             action.move_to_element(icon).perform()
