83ab77507ffdb383377ba5e0f1b5dfda766d7fc8
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-09-17 11:20:08 +0700

    Update App

--- app.py
+++ app.py
@@ -305,6 +305,7 @@ def download_icon_vnpt(driver, action, wait, temp_folder):
         st.write_stream(stream_data("Đang tải hóa đơn ..."))
         for icon in icons:
 
+
             # Move to the icon and click to open details
             action.move_to_element(icon).perform()
             driver.execute_script("arguments[0].click();", icon)
@@ -315,21 +316,25 @@ def download_icon_vnpt(driver, action, wait, temp_folder):
                 EC.presence_of_element_located((By.XPATH, "//div[@id='taiXml']"))
             )
 
-            # If button is found, click to download the invoice
-            driver.execute_script("arguments[0].click();", download_button)
-            time.sleep(3)
+            if download_button:
+                # If button is found, click to download the invoice
+                driver.execute_script("arguments[0].click();", download_button)
+                time.sleep(3)
+
 
             # Check if the file has been downloaded
-            downloaded_file = wait_for_download(temp_folder)
-            if downloaded_file:
-                # Extract and rename the downloaded file(s)
-                shd = extract_number_vnpt(os.path.basename(downloaded_file))
-                extracted_files = extract_zipfile(downloaded_file, temp_folder)
-                for file in extracted_files:
+                downloaded_file = wait_for_download(temp_folder)
+                if downloaded_file:
+                    # Extract and rename the downloaded file(s)
+                    shd = extract_number_vnpt(os.path.basename(downloaded_file))
+                    extracted_files = extract_zipfile(downloaded_file, temp_folder)
+                    for file in extracted_files:
                     xml_file = shd + file[file.index('.xml'):]
                     xml_files.append((xml_file, file))
                     os.rename(os.path.join(temp_folder, file), os.path.join(temp_folder, xml_file))
-
+            else:
+                st.write_stream(stream_data("Tìm thấy hóa đơn chưa phát hành"))
+                pass
             # After downloading, close the modal popup
             close_button = wait.until(
                 EC.presence_of_element_located((By.XPATH, "//button[@class='close']"))
