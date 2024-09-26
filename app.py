2630bd802c74a91ce9185e22a87b9e31862f1831
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-09-27 03:36:20 +0700

    Update App

--- app.py
+++ app.py
@@ -556,6 +556,7 @@ def handle_viettel_download(driver, action, wait, user, start_date, end_date, te
             #     EC.presence_of_element_located((By.XPATH, "//button[span[text()='Tìm kiếm']]"))
             # )
             search_btn = driver.find_element(By.XPATH, "//button[span[text()='Tìm kiếm']]")
+            st.write(search_btn)
             search_btn.click()
             st.write_stream(stream_data(("Đang tìm hóa đơn ...")))
 
