0de1d7a3c8c8cf4a28af097dfcfc0e5b6d196ee3
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-09-27 03:59:30 +0700

    Update App

--- app.py
+++ app.py
@@ -569,8 +569,11 @@ def handle_viettel_download(driver, action, wait, user, start_date, end_date, te
                     xml_files = download_icon_viettel(driver, action, wait, temp_folder)
                     if xml_files:
                         final_xml_files.update(xml_files)
+                    # next_button = wait.until(
+                    #     EC.element_to_be_clickable((By.XPATH, "//a[@aria-label='Next' and contains(@class, 'page-link')]"))
+                    # )
                     next_button = wait.until(
-                        EC.element_to_be_clickable((By.XPATH, "//a[@aria-label='Next' and contains(@class, 'page-link')]"))
+                        EC.element_to_be_clickable((By.XPATH, "//a[@aria-label='Next']"))
                     )
                     st.write(next_button)
                     driver.execute_script("arguments[0].scrollIntoView(true);", next_button)
